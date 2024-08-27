bad7e01a0249480b911be96a7e15d004c1abea80
Rutuja Bahadure <bahadurerutu558@gamil.com>
2024-08-27 12:40:29 +0530

    mbmb

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,19 +2,19 @@ const asyncHandler = require("express-async-handler")
 const Todo = require("../model/Todo")
 
 
-exports.createTodo = asyncHandler(async () => {
+exports.createTodo = asyncHandler(async (req, res) => {
     await Todo.create(req.body)
     res.json({ message: "createTodo Success" })
 })
-exports.readTodo = asyncHandler(async () => {
+exports.readTodo = asyncHandler(async (req, res) => {
     const result = await Todo.find()
     res.json({ message: "readTodo Success", result })
 })
-exports.updateTodo = asyncHandler(async () => {
+exports.updateTodo = asyncHandler(async (req, res) => {
     await Todo.findByIdAndUpdate(req.params.id, req.body)
     res.json({ message: "updateTodo Success" })
 })
-exports.deleteTodo = asyncHandler(async () => {
+exports.deleteTodo = asyncHandler(async (req, res) => {
     await Todo.findByIdAndDelete(req.params.id)
     res.json({ message: "deleteTodo Success" })
 })
